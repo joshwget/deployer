0bd57cc37a6b740c039f59c797f990c4b213c10a
Josh Curl <josh@curl.me>
2020-03-14 21:01:46 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-url='https://gist.githubusercontent.com/joshwget/ef699d68f6a4f8d2bbecf2135633e98d/raw/af58638f0d42dedd4fcbc2247d5eda9b1d0571ab/docker-compose.yml'
+url='http://jc-deployer.s3-website-us-west-2.amazonaws.com/docker-compose.yml'
 
 while true
 do
